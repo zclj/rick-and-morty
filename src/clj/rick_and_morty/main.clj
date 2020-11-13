47556df9e92cd65fc21275c62d794eb6e674e352
Ola Ottemalm <ola.ottemalm@se.abb.com>
2020-11-13 15:25:06 +0100

    Added serving of static files

--- src/clj/rick_and_morty/main.clj
+++ src/clj/rick_and_morty/main.clj
@@ -1,29 +1,49 @@
 (ns rick-and-morty.main
-  (:require [io.pedestal.http :as http]))
+  (:require [clojure.pprint :refer [pprint]]
+            [io.pedestal.http :as http]))
 
 (defn hello
   [request]
   {:status 200
-   :body "Rick and Morty app!"})
+   :body "Rick and Morty app!!!"})
 
-(defn echo
-  [request]
-  {:status 200
-   :body "Should echo request here"})
+(def echo
+  {:name :echo
+   :enter (fn [context]
+            (let [response {:status 200
+                            :headers {"Content-Type" "text/plain"}
+                            :body (with-out-str (pprint context))}]
+              (assoc context :response response)))})
 
-(def routes #{["/"     :get hello :route-name ::root]
-              ["/echo" :any echo  :route-name ::echo]})
+(def routes #{["/"     :get #'hello :route-name ::root]
+              ["/echo" :any #'echo  :route-name ::echo]})
+
+(defonce instance (atom nil))
 
 (defn start
+  ([] (start false))
+  ([join?]
+   (when (nil? @instance)
+     (let [server-map {::http/type          :jetty
+                       ::http/host          "0.0.0.0"
+                       ::http/port          80
+                       ::http/join?         join?
+                       ::http/routes        routes
+                       ::http/resource-path "/public"}
+           server (http/create-server server-map)]
+       (reset! instance (http/start server))))))
+
+(defn stop
   []
-  (let [server-map {::http/type   :jetty
-                    ::http/host   "0.0.0.0"
-                    ::http/port   80
-                    ::http/join?  true
-                    ::http/routes routes}
-        server (http/create-server server-map)]
-    (http/start server)))
+  (when (some? @instance)
+    (http/stop (deref instance))
+    (reset! instance nil)))
+
+(defn reset
+  []
+  (stop)
+  (start))
 
 (defn -main
   [& args]
-  (start))
+  (start true))
